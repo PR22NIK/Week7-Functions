d9576aa517894a17c128d2deb910599d46decbf6
PR22NIK <90636193+PR22NIK@users.noreply.github.com>
2021-12-22 17:36:19 +0200

    Update Program.cs

--- UserNameDataTask/Program.cs
+++ UserNameDataTask/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace UserNameDataTask
 {
@@ -6,7 +6,51 @@ namespace UserNameDataTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //programm kusib kasutajal sisestada eesnime;
+            //programm kusib kasutajal sisestad numbri 1-3
+            //kui kasutaja sisestab '1', siis kuvatakse kasutaja eesnime tagurpidi
+            //kui kasutaja valib '2', siis kuvatakse kasutaja eesnime esimest tahte
+            //kui kasutaja valib '3', siis kuvatakse kasutaja eesnime pikkust
+
+            Console.WriteLine("Siesta oma eesnimi:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Vali number 1-3");
+            int userAnswer = Convert.ToInt32(Console.ReadLine());
+
+            if (userAnswer == 1)
+            {
+                GetUserFirstData(userName);
+            }
+            else if (userAnswer == 2)
+            {
+                GetUserSecondData(userName);
+            }
+
+            else if (userAnswer == 3)
+            {
+                GetUserThirdData(userName);
+            }
+
         }
+
+        private static void GetUserFirstData(string userInput)
+        {
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+
+        public static void GetUserSecondData(string userInput)
+        {
+            Console.WriteLine($"Sinu eesnime esimene täht on {userInput[0]}");
+        }
+
+        public static void GetUserThirdData(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sumbolit pikk.");
+        }
+
     }
+}
 }
